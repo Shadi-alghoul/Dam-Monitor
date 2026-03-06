2bc90e79a79d72c53491069a2379506d3be6b165
Shadi-alghoul <r1032259@ucll.be>
2026-03-06 13:34:20 +0100

    initial backend project

---
+++ dam-monitor-backend/src/main/java/ucll/be/dammonitorbackend/DamMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package ucll.be.dammonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamMonitorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamMonitorBackendApplication.class, args);
+    }
+
+}
---
+++ dam-monitor-backend/src/test/java/ucll/be/dammonitorbackend/DamMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ucll.be.dammonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamMonitorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
