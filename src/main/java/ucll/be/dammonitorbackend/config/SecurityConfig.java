c0556d618b9c6a14062f6b63af39098f77a4f311
Armand Vermeulen <vermeulen.armand12@gmail.com>
2026-03-16 17:18:51 +0200

    Earth engine API in progess

--- src/main/java/ucll/be/dammonitorbackend/config/SecurityConfig.java
+++ src/main/java/ucll/be/dammonitorbackend/config/SecurityConfig.java
@@ -20,10 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated())
-                .httpBasic()
-                .and()
+                        .anyRequest().permitAll())
                 .csrf().disable()
                 .headers().frameOptions().disable();
 
